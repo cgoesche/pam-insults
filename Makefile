2092fc2a94b7c2364458983fcb0d06370eb974c7
cgoesche <cgoesc2@wgu.edu>
2025-05-10 17:34:31 -0400

    fix: improve po/pomgr.sh

--- Makefile
+++ Makefile
@@ -1,26 +1,22 @@
 CC = gcc
 CFLAGS = -fPIC -Wall -g -O2
 
-MODULE = pam_insults
-VERSION = 0.2.1-beta
+PACKAGE_NAME = pam-insults
+PACKAGE_VERSION = $(shell head -n1 VERSION)
 
+PAM_MODULE_NAME = pam_insults
 PAM_MODULES_DIR ?= $(or $(wildcard /usr/lib64/security/.),/usr/lib/security/.)
-MODULES_DIR = modules/
-SOURCES_DIR = src/
-MAN_DATABASE ?= /usr/share/man/man8/
-MAN_DIR = man/
+MODULES_DIR = modules
+SOURCES_DIR = src
+MAN_DATABASE ?= /usr/share/man/man8
+MAN_DIR = man
 
-SOURCES = $(SOURCES_DIR)$(MODULE).c
+SOURCES = $(SOURCES_DIR)/$(PAM_MODULE_NAME).c
 OBJECTS = $(SOURCES:.c=.o)
-SHARED_OBJECT = $(MODULES_DIR)$(MODULE).so
-MANPAGE_GZ = $(MAN_DIR)$(MODULE).8.gz
+SHARED_OBJECT = $(MODULES_DIR)/$(PAM_MODULE_NAME).so
+MANPAGE_GZ = $(MAN_DIR)/$(PAM_MODULE_NAME).8.gz
 
-XGETTEXT = xgettext
-XGETTEXT_FLAGS = -F -k_ -kN_ --copyright-holder="Christian Goeschel Ndjomouo" \
-		"--msgid-bugs-address=https://github.com/cgoesche/pam-insults/issues" \
-		--package-name=pam-insults --package-version=$(VERSION) \
-		--language=C --add-comments
-
+PO_DIR = po
 
 all: $(SHARED_OBJECT) manpage
 
@@ -32,27 +28,14 @@ $(OBJECTS): $(SOURCES)
 	$(CC) $(CFLAGS) -c $< -o $@
 
 manpage:
-	asciidoctor -b manpage -a release-version=$(VERSION) -a adjtime_path=/etc/adjtime $(MANPAGE_GZ:.gz=.adoc)
-	gzip --suffix=.gz -f $(MAN_DIR)$(MODULE).8
-
-update-pot:
-	$(XGETTEXT) $(XGETTEXT_FLAGS) -o po/pam-insults.pot -f po/POTFILES.in
-
-update-po:
-	cd po/
-	chmod +x pomgr.sh && ./pomgr.sh update-po
-	cd ..
-
-compile-po: update-po
-	./pomgr.sh compile
+	asciidoctor -b manpage -a release-version=$(PACKAGE_VERSION) -a adjtime_path=/etc/adjtime $(MANPAGE_GZ:.gz=.adoc)
+	gzip --suffix=.gz -f $(MAN_DIR)/$(PAM_MODULE_NAME).8
 
 install: $(SHARED_OBJECT) $(MANPAGE_GZ)
-	[ ! -f $(PAM_MODULES_DIR:.=)$(MODULE).so ] || sudo rm $(PAM_MODULES_DIR:.=)$(MODULE).so
+	[ ! -f $(PAM_MODULES_DIR:.=)/$(PAM_MODULE_NAME).so ] || sudo rm $(PAM_MODULES_DIR:.=)/$(PAM_MODULE_NAME).so
 	sudo cp $(SHARED_OBJECT) $(PAM_MODULES_DIR)
 	sudo cp $(MANPAGE_GZ) $(MAN_DATABASE)
-	mandb
+	sudo mandb
 
 clean:
 	rm -f $(OBJECTS) $(SHARED_OBJECT)
-
-
--- po/pomgr.sh
+++ po/pomgr.sh
@@ -1,24 +1,116 @@
 #!/bin/bash
 
+declare -r PACKAGE="pam-insults"
+declare -r VERSION="$(head -n1 ../VERSION 2>/dev/null)"
+declare -r LOCALE_DIR="/usr/share/locale"
+
+declare -r POTFILE="pam-insuls.pot"
+declare -r POTFILES_IN="POTFILES.in"
+
+function usage(){
+cat <<EOF
+pomgr - Manage po files for GNU gettext translations
+
+Syntax: pomgr ARG
+
+Arguments:
+
+clean           Remove .mo files and/or old .po~ files
+compile LOCALE  Compile a specific .po file, specified with the LOCALE
+compile-all     Compile all .po files
+gen-po          Generate .po files from the template file \'"${POTFILE}"\'
+help            Print usage information
+install         Install all compiled .mo files on the system
+update-po       Merge changes in the template into the exisiting .po files
+update-pot      Create or update an existing POT template \(\'"${POTFILE}"\'\)
+
+EOF
+}
+
+function compile() {
+        local locale="${1}"
+
+        if [ ! -f "${locale}.po" ]; then 
+                echo "Cannot compile .mo file for LOCALE=${locale}: No such file ./${locale}.po"
+                return 1
+        fi
+
+        msgfmt --output-file="${locale}.mo" "${locale}.po"
+}
+
+function compile_all() {
+        while read -r po_file; do
+                msgfmt --output-file="${po_file/.po/.mo}" "${po_file}"
+        done < <(ls -F1 -- *.po)
+}
+
 function gen_po() {
         while read -r locale; do
-                msginit --no-translator --input=pam-insults.pot --locale="${locale}" --output="${locale}".po
+                msginit --no-translator --input="${POTFILE}" --locale="${locale}" --output="${locale}".po
         done < "LINGUAS"
 }
 
-function compile_po() {
-        while read -r pofile; do
-                msgfmt --output-file="${pofile/.po/.mo}" "${pofile}"
+function _install() {
+        while read -r mo_file; do
+                local locale_dir="${LOCALE_DIR}/${mo_file/.mo/}/LC_MESSAGES"
+                
+                if [ ! -d "${locale_dir}" ]; then
+                        echo "${locale_dir} not found ... skipping installation for ${mo_file}" 
+                        continue
+                fi
+
+                echo "Installing ${mo_file} -> ${locale_dir}" 
+                if [ -f "${locale_dir}/${mo_file}" ]; then
+                        echo "Removing existing ${locale_dir}/${mo_file}"
+                        rm -f "${locale_dir}/${mo_file}"
+                fi
+
+                if ! cp "${mo_file}" "${locale_dir}"/ ; then
+                        echo "Failed installation for ${mo_file}"
+                        continue
+                fi
+                echo "Installed ${mo_file}"
+
+        done < <(ls -F1 -- *.mo)
+}
+
+function update_po() {
+        while read -r po_file; do
+                msgmerge --update "${po_file}" "${POTFILE}"
         done < <(ls -F1 -- *.po)
 }
 
 case ${1} in
+        "clean")
+                rm -f -- ./*.mo ./*~
+        ;;
+        "compile")
+                compile "${2}"
+        ;;
+        "compile-all")
+                compile_all
+        ;;
         "gen-po")
                 gen_po
         ;;
-        "compile")
-                compile_po
+        "help")
+                usage
+        ;;
+        "install")
+                _install
+        ;;
+        "update-po")
+                update_po
+        ;;
+        "update-pot")
+                xgettext -F -k_ -kN_ --copyright-holder='Christian Goeschel Ndjomouo' \
+		'--msgid-bugs-address=https://github.com/cgoesche/pam-insults/issues' \
+		--package-name=pam-insults --package-version="${VERSION}" \
+		--language=C --add-comments -o "${POTFILE}" -f "${POTFILES_IN}"
         ;;
         *)      
-                update_pot
+                usage
+                exit 1
 esac
+
+exit 0
